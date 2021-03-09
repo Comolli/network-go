4278056119a3f22ec4c6ccc3ebc16d91f79d3db6
liangweicheng2@gmail.com <liangweicheng2@gmail.com>
2021-03-09 15:39:09 +0800

    add prefork

--- network_go/socket.go
+++ network_go/socket.go
@@ -81,6 +81,13 @@ func newSocketCloexecOld(domain, typ, proto int) (int, error) {
 	return fd, nil
 }
 
+func enableDeferAccept(fd int) error {
+	if err := syscall.SetsockoptInt(fd, syscall.IPPROTO_TCP, syscall.TCP_DEFER_ACCEPT, 1); err != nil {
+		return fmt.Errorf("cannot enable TCP_DEFER_ACCEPT: %s", err)
+	}
+	return nil
+}
+
 func enableFastOpen(fd int) error {
 	if err := syscall.SetsockoptInt(fd, syscall.SOL_TCP, tcpFastOpen, fastOpenQlen); err != nil {
 		return fmt.Errorf("cannot enable TCP_FASTOPEN(qlen=%d): %s", fastOpenQlen, err)
